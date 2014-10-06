f60be0e47bcf5ba9a9efb9b18300752aeff0e3f9
Kate Windheuser <roboticbrain@gmail.com>
2014-10-06 17:23:11 -0600

    FINALLY got the data.js file working, but it takes 2 min to load.

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 $(document).on('ready', function() {
-  
+  var tableData = dataPrettier();
+	createTableRows(tableData);
+	$('#datatable').DataTable({
+		"pageLength":20,
+		"lengthChange": false
+	});
 });
 
+function createTableRows (dataArray) {
+	var tableData = dataArray;
+	for (var i = 0; i < dataArray.length; i++){
+		var currentRow = dataArray[i];
+		var gameName = currentRow.productname;
+		var consoleName = currentRow.consolename;
+		var genre = currentRow.genre;
+		var loose = currentRow.looseprice;
+		var cib = currentRow.cibprice;
+		var newrow = '<tr><td>'+gameName+'</td><td>'+consoleName+'</td><td>'+genre+'</td><td>$'+loose+'</td><td>$'+cib+'</td></tr>';
+
+		$('.tablebody').append(newrow);
+	};
+};
